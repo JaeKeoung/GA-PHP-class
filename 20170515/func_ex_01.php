5d67ac437d0a345cd62681a8ef7181602ceefa41
ykchae55 <itpaper.co.kr@gmail.com>
2017-05-15 22:21:34 +0900

    2주차 월요일

--- 20170515/func_ex_01.php
+++ 20170515/func_ex_01.php
@@ -14,6 +14,26 @@ print "<br>";
 a(1,2,3,4,5,null);
 print "<br>";
 a(1,2,3,4,5,6);
+print "<br>";
 
-//결과 다 잘 나온다.
+// 결과 다 잘 나온다.
+
+// 재귀함수(무한 루프)
+function test($n) {
+	$c = $n;
+	if ($n == 1) {
+		return 1;
+	}
+	$c += test($n - 1);
+	
+	return $c;
+	// 이러케 하면 메모리 터져서 뒤져버림. 아마도 뒤졌어야했는데...
+}
+function test_test($param) {
+	if (true) {
+		return $param;
+	}
+}
+print test(10);
+
 ?>
